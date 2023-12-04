a5213572dab2a72d1790168bfba0dbf9607b13f3
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2023-12-04 11:42:11 -0300

    criacao do metodo post client

--- desafiocrud/src/main/java/com/springprofissional/desafiocrud/services/ClientService.java
+++ desafiocrud/src/main/java/com/springprofissional/desafiocrud/services/ClientService.java
@@ -29,4 +29,20 @@ public class ClientService {
         Page<Client> result = repository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client client = new Client();
+        CopyDtoEntity(dto,client);
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
+
+    public void CopyDtoEntity(ClientDTO dto,Client entity){
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setChildren(dto.getChildren());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+    }
 }
